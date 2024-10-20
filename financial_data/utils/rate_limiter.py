d4573a74cad20c9471f2feb4707a35e3d2fddbbb
raj921 <raj315920@gmail.com>
2024-10-20 13:56:47 +0530

    Initial commit

--- financial_data/utils/rate_limiter.py
+++ financial_data/utils/rate_limiter.py
@@ -2,14 +2,7 @@ import time
 from django.core.cache import cache
 
 def rate_limit(key, limit, period):
-    """
-    Rate limiter using Django's cache framework.
-    
-    :param key: Unique identifier for the rate limit (e.g., API endpoint name)
-    :param limit: Number of allowed requests per period
-    :param period: Time period in seconds
-    :return: True if request is allowed, False if rate limit is exceeded
-    """
+   
     current_time = int(time.time())
     cache_key = f"rate_limit:{key}:{current_time // period}"
     
--- stock_analyzer/urls.py
+++ stock_analyzer/urls.py
@@ -5,5 +5,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('financial_data.urls')),
-    path('', RedirectView.as_view(url='/api/', permanent=False)),  # Add this line
+    path('', RedirectView.as_view(url='/api/', permanent=False)),  
 ]
