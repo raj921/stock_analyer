a7ceb0ba80d293126296380b031857f004816147
raj kumar <79806602+raj921@users.noreply.github.com>
2024-12-11 16:01:21 +0530

    Update DateTimeShortcuts.js

--- staticfiles/admin/js/admin/DateTimeShortcuts.js
+++ staticfiles/admin/js/admin/DateTimeShortcuts.js
@@ -1,3 +1,7 @@
+
+
+
+
 /*global Calendar, findPosX, findPosY, get_format, gettext, gettext_noop, interpolate, ngettext, quickElement*/
 // Inserts shortcut buttons after all of the following:
 //     <input type="text" class="vDateField">
